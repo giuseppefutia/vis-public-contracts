a0f9470075b10cb0d84cd6c4091bd8de873f1387
alemela <alessiom92@gmail.com>
2015-05-26 21:47:10 +0200

    Fixed CORS

--- server/server.js
+++ server/server.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(express.static('../'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/test', function (request, response) {
     PC.launchSparqlQuery(request, response, PC.test, "text/turtle");
 });
