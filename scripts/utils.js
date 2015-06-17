89190b54f075b744c42e2ca19d8a724c586a2f02
alemela <alessiom92@gmail.com>
2015-06-17 21:56:06 +0200

    Fixed stats view

--- scripts/utils.js
+++ scripts/utils.js
@@ -84,3 +84,17 @@ if (location.search !== "") {
     var viewID = location.search.replace('?', '');
     loadAll(viewID);
 }
+
+/* check if stats file exists */
+function doesFileExist(urlToFile)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
