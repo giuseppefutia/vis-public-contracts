4c53bf17ddcebe26278032db02182f153c5a5a74
alemela <alessiom92@gmail.com>
2015-06-18 08:16:59 +0200

    Fixed smooth navigation

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,5 @@
 /* smooth scrolling for nav sections */
-$('.nav a').click(function(){
+$('.smooth').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top -50
     }, 500);
